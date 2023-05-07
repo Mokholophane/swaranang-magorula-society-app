1360f3b21b452a43ed39fe0387f6840f5f213101
Merciful <messymokholophaneunite@gmail.com>
2023-05-07 20:42:13 +0200

    Added missed files to Git.

---
+++ src/main/java/com/swaranangmagorulasociety/SwaranangMagorulaSocietyDemoApplication.java
@@ -0,0 +1,15 @@
+package com.swaranangmagorulasociety;
+
+import com.swaranangmagorulasociety.model.Member;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaranangMagorulaSocietyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaranangMagorulaSocietyDemoApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/swaranangmagorulasociety/SwaranangMagorulaSocietyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaranangmagorulasociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaranangMagorulaSocietyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
